f785b400ba2097bba53c5bca6e293cecd363be49
ASWINKUMAR-P <aswi20113.cs@rmkec.ac.in>
2021-11-11 10:39:50 +0530

    Back and exit button added and enabled in maincode

--- maincode.py
+++ maincode.py
@@ -19,6 +19,10 @@ try:
     cur.execute("create table userdetails(username varchar(50) primary key, emailid varchar(50) not null, phone varchar(15) not null, pass varchar(50) not null, incomepermonth int not null)")
 except:
     pass
+###########################################################################################################################################
+def gotoHome(page):
+    page.destroy()
+    Home()
 #############################################################################################################################################
 def displayexpense(dash,username):
     dash.destroy()
@@ -28,7 +32,7 @@ def displayexpense(dash,username):
     display.title("PET Login or Signup")
     head=tk.Label(display,text="Personal Expense Tracker",pady=10,font=("Impact",20),background="red",width=40).grid(row=0,column=0,columnspan=4)
     empty1=tk.Label(display,text=" ").grid(row=1,column=0,columnspan=4)
-    query="select* from "+username
+    query="select* from "+username+" order by dateofexpense desc"
     try:
         cur.execute(query)
         rows=cur.fetchall()
@@ -139,7 +143,7 @@ def create(signup,nameInput,emailInput,pnoInput,incomeInput,pwInput,cpwInput):
     signup.destroy()
     dashboard(name)
 #############################################################################################################################################################
-def login(username,password):
+def login(home,username,password):
     un=username.get()
     pw=password.get()
     if not un or not pw:
@@ -169,7 +173,7 @@ def reset(nameInput,emailInput,pnoInput,incomeInput,pwInput,cpwInput):
     cpwInput.delete(0,END)
 ###########################################################################################################################################
 #Signup page
-def signuppage():
+def signuppage(home):
 
     home.destroy()
     signup=tk.Tk()
@@ -215,44 +219,52 @@ def dashboard(username):
     dash.title(username)
     headLabel=tk.Label(dash,text="PERSONAL EXPENSE TRACKER",background="red",pady=10,font=("Impact",20),width=40).grid(row=0,column=0,columnspan=2)
     empty1=tk.Label(dash,text=" ").grid(row=1,column=0)
-    Label1= tk.Label(dash,text="Add an expense:- ",font=("Times New Roman",16)).grid(row=2,column=0,sticky='w')
+    Label1= tk.Label(dash,text="    Add an expense:- ",font=("Times New Roman",16)).grid(row=2,column=0,sticky='w')
     Insert=tk.Button(dash,text="Enter",padx=10,width=7,bg="red",font=("Times New Roman",12),command=lambda: enterexpense(dash,username)).grid(row=2,column=1)
     empty2=tk.Label(dash,text=" ").grid(row=3,column=0)
-    Label2= tk.Label(dash,text="Show all the expenses:- ",font=("Times New Roman",16)).grid(row=4,column=0,sticky='w')
+    Label2= tk.Label(dash,text="    Show all the expenses:- ",font=("Times New Roman",16)).grid(row=4,column=0,sticky='w')
     Showall=tk.Button(dash,text="Enter",padx=10,width=7,bg="red",font=("Times New Roman",12),command=lambda: displayexpense(dash,username)).grid(row=4,column=1)
     empty3=tk.Label(dash,text=" ").grid(row=5,column=0)
-    Label3= tk.Label(dash,text="Show all expenses by day:- ",font=("Times New Roman",16)).grid(row=6,column=0,sticky='w')
+    Label3= tk.Label(dash,text="    Show all expenses by day:- ",font=("Times New Roman",16)).grid(row=6,column=0,sticky='w')
     ShowByDay=tk.Button(dash,text="Enter",padx=10,width=7,bg="red",font=("Times New Roman",12)).grid(row=6,column=1)
     empty3=tk.Label(dash,text=" ").grid(row=7,column=0)
-    Label4= tk.Label(dash,text="Show all expenses by month:- ",font=("Times New Roman",16)).grid(row=8,column=0,sticky='w')
+    Label4= tk.Label(dash,text="    Show all expenses by month:- ",font=("Times New Roman",16)).grid(row=8,column=0,sticky='w')
     ShowByMonth=tk.Button(dash,text="Enter",padx=10,width=7,bg="red",font=("Times New Roman",12)).grid(row=8,column=1)
     empty3=tk.Label(dash,text=" ").grid(row=9,column=0)
+    Label5= tk.Label(dash,text="    Back to login page:- ",font=("Times New Roman",16)).grid(row=10,column=0,sticky='w')
+    Back=tk.Button(dash,text="Enter",padx=10,width=7,bg="red",font=("Times New Roman",12),command=lambda:gotoHome(dash)).grid(row=10,column=1)
+    empty4=tk.Label(dash,text=" ").grid(row=11,column=0)
+    Label6= tk.Label(dash,text="    Exit:- ",font=("Times New Roman",16)).grid(row=12,column=0,sticky='w')
+    Exit=tk.Button(dash,text="Enter",padx=10,width=7,bg="red",font=("Times New Roman",12),command=lambda: sys.exit(0)).grid(row=12,column=1)
+    empty5=tk.Label(dash,text=" ").grid(row=13,column=0)
     dash.mainloop()
 ####################################################################################################################################
-#Login Page
-home=tk.Tk()
-home.geometry('500x500')
-home.resizable(False,False)
-home.title("PET Login or Signup")
-head=tk.Label(home,text="Personal Expense Tracker",pady=10,font=("Impact",20),background="red",width=40).grid(row=0,column=0,columnspan=2)
-empty1=tk.Label(home,text=" ").grid(row=1,column=0,columnspan=2)
-Label1=tk.Label(home,text="Enter username:-",font=("Times New Roman",16)).grid(row=2,column=0)
-Username=tk.Entry(home,width=20,font=("Times New Roman",16))
-Username.grid(row=2,column=1)
-empty2=tk.Label(home,text=" ").grid(row=3,column=0,columnspan=2)
-Label2=tk.Label(home,text="Enter password:-",font=("Times New Roman",16)).grid(row=4,column=0)
-Password=tk.Entry(home,width=20,font=("Times New Roman",16),show="*")
-Password.grid(row=4,column=1)
-empty2=tk.Label(home,text=" ").grid(row=5,column=0,columnspan=2)
-Login=tk.Button(home,text="Login",padx=10,width=5,bg="red",command=lambda: login(Username,Password)).grid(row=6,column=0,columnspan=2)
-empty3=tk.Label(home,text=" ").grid(row=7,column=0,columnspan=2)
-orlabel=tk.Label(home,text="or",font=("Times New Roman",16)).grid(row=8,column=0,columnspan=2)
-empty4=tk.Label(home,text=" ").grid(row=9,column=0,columnspan=2)
-Label3=tk.Label(home,text="New user, click sign up button",font=("Times New Roman",16)).grid(row=10,column=0,columnspan=2)
-empty5=tk.Label(home,text=" ").grid(row=11,column=0,columnspan=2)
-Signup=tk.Button(home,text="Sign up",padx=10,width=5,bg="red",command=signuppage).grid(row=12,column=0,columnspan=2)
+#Home page
+def Home():
+    home=tk.Tk()
+    home.geometry('500x500')
+    home.resizable(False,False)
+    home.title("PET Login or Signup")
+    head=tk.Label(home,text="Personal Expense Tracker",pady=10,font=("Impact",20),background="red",width=40).grid(row=0,column=0,columnspan=2)
+    empty1=tk.Label(home,text=" ").grid(row=1,column=0,columnspan=2)
+    Label1=tk.Label(home,text="Enter username:-",font=("Times New Roman",16)).grid(row=2,column=0)
+    Username=tk.Entry(home,width=20,font=("Times New Roman",16))
+    Username.grid(row=2,column=1)
+    empty2=tk.Label(home,text=" ").grid(row=3,column=0,columnspan=2)
+    Label2=tk.Label(home,text="Enter password:-",font=("Times New Roman",16)).grid(row=4,column=0)
+    Password=tk.Entry(home,width=20,font=("Times New Roman",16),show="*")
+    Password.grid(row=4,column=1)
+    empty2=tk.Label(home,text=" ").grid(row=5,column=0,columnspan=2)
+    Login=tk.Button(home,text="Login",padx=10,width=5,bg="red",command=lambda: login(home,Username,Password)).grid(row=6,column=0,columnspan=2)
+    empty3=tk.Label(home,text=" ").grid(row=7,column=0,columnspan=2)
+    orlabel=tk.Label(home,text="or",font=("Times New Roman",16)).grid(row=8,column=0,columnspan=2)
+    empty4=tk.Label(home,text=" ").grid(row=9,column=0,columnspan=2)
+    Label3=tk.Label(home,text="New user, click sign up button",font=("Times New Roman",16)).grid(row=10,column=0,columnspan=2)
+    empty5=tk.Label(home,text=" ").grid(row=11,column=0,columnspan=2)
+    Signup=tk.Button(home,text="Sign up",padx=10,width=5,bg="red",command=lambda: signuppage(home)).grid(row=12,column=0,columnspan=2)
+    home.mainloop()
 #######################################################################################################################################
-home.mainloop()
+Home()
 
 
 
