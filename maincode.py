7b93bfcc4fe017d41e7063d9e985c0e6baa2fbd8
ASWINKUMAR-P <aswi20113.cs@rmkec.ac.in>
2021-11-11 11:39:39 +0530

    Scroll bar enabled

--- maincode.py
+++ maincode.py
@@ -1,8 +1,8 @@
 import tkinter as tk
-from tkinter import messagebox,StringVar
+from tkinter import messagebox,StringVar,ttk
 import datetime
 import re
-from tkinter.constants import END
+from tkinter.constants import BOTH, END, LEFT, RIGHT, VERTICAL, Y
 import mysql.connector as sqlc
 import sys
 import tkcalendar as tkc
@@ -27,33 +27,42 @@ def gotoHome(page):
 def displayexpense(dash,username):
     dash.destroy()
     display=tk.Tk()
-    display.geometry('510x510')
+    display.geometry('550x550')
     display.resizable(False,False)
-    display.title("PET Login or Signup")
-    head=tk.Label(display,text="Personal Expense Tracker",pady=10,font=("Impact",20),background="red",width=40).grid(row=0,column=0,columnspan=4)
-    empty1=tk.Label(display,text=" ").grid(row=1,column=0,columnspan=4)
-    query="select* from "+username+" order by dateofexpense desc"
+    display.title("Expense details")
+    main_frame=ttk.Frame(display)
+    main_frame.pack(fill=BOTH,expand=1)
+    my_canvas=tk.Canvas(main_frame)
+    my_canvas.pack(side=LEFT,fill=BOTH,expand=1)
+    my_scrollbar=ttk.Scrollbar(main_frame,orient=VERTICAL,command=my_canvas.yview)
+    my_scrollbar.pack(side=RIGHT,fill=Y)
+    my_canvas.configure(yscrollcommand=my_scrollbar.set)
+    my_canvas.bind('<Configure>',lambda e:my_canvas.configure(scrollregion=my_canvas.bbox("all")))
+    second_frame=ttk.Frame(my_canvas)
+    my_canvas.create_window((0,0),window=second_frame,anchor="nw")
+    head=tk.Label(second_frame,text="Personal Expense Tracker",pady=10,font=("Impact",20),background="red",width=40).grid(row=0,column=0,columnspan=4)
+    empty1=tk.Label(second_frame,text=" ").grid(row=1,column=0,columnspan=4)
+    query="select* from "+username+" order by dateofexpense"
     try:
         cur.execute(query)
         rows=cur.fetchall()
     except:
         print("Error")
-    col1=tk.Label(display,text="S.No",font=("Times New Roman",16)).grid(row=2,column=0)
-    col2=tk.Label(display,text="Date",font=("Times New Roman",16)).grid(row=2,column=1)
-    col3=tk.Label(display,text="Expense name",font=("Times New Roman",16)).grid(row=2,column=2)
-    col4=tk.Label(display,text="Price",font=("Times New Roman",16)).grid(row=2,column=3)
-    empty1=tk.Label(display,text=" ").grid(row=3,column=0,columnspan=4)
+    col1=tk.Label(second_frame,text="S.No",font=("Times New Roman",16)).grid(row=2,column=0)
+    col2=tk.Label(second_frame,text="Date",font=("Times New Roman",16)).grid(row=2,column=1)
+    col3=tk.Label(second_frame,text="Expense name",font=("Times New Roman",16)).grid(row=2,column=2)
+    col4=tk.Label(second_frame,text="Price",font=("Times New Roman",16)).grid(row=2,column=3)
+    empty1=tk.Label(second_frame,text=" ").grid(row=3,column=0,columnspan=4)
     j=0
     k=0
     for i in (rows):
-        r0=tk.Label(display,text=str(k+1)+".",font=("Times New Roman",16)).grid(row=j+4,column=0)
-        r1=tk.Label(display,text=i[2],font=("Times New Roman",16)).grid(row=j+4,column=1)
-        r2=tk.Label(display,text=i[0],font=("Times New Roman",16)).grid(row=j+4,column=2)
-        r3=tk.Label(display,text=i[1],font=("Times New Roman",16)).grid(row=j+4,column=3)
-        e=tk.Label(display,text=" ").grid(row=j+2,column=0,columnspan=4)
+        r0=tk.Label(second_frame,text=str(k+1)+".",font=("Times New Roman",16)).grid(row=j+4,column=0)
+        r1=tk.Label(second_frame,text=i[2],font=("Times New Roman",16)).grid(row=j+4,column=1)
+        r2=tk.Label(second_frame,text=i[0],font=("Times New Roman",16)).grid(row=j+4,column=2)
+        r3=tk.Label(second_frame,text=i[1],font=("Times New Roman",16)).grid(row=j+4,column=3)
+        e=tk.Label(second_frame,text=" ").grid(row=j+2,column=0,columnspan=4)
         j=j+2
         k=k+1
-
     display.mainloop()
 ######################################################################################################################################################################
 def submitexpense(enter,username,expensenameInput,expenseamountInput,expensedateInput):
